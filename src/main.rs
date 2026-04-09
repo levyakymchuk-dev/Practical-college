bf61906349eac6f1aca0bafe3a1099495583d16f
Lev <levyakym4uk@gmail.com>
2026-04-09 09:33:20 +0300

    Practice 2 completed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    println!("--- Section 3 ---");
+    section3();
+
+    println!("--- Section 4 ---");
+    section4();
+
+    println!("--- Section 6 ---");
+    section6();
+}
+
+fn section3() {
+    let x = 5;
+    println!("x = {}", x);
+}
+
+fn section4() {
+    let x = 5;
+    let x = x + 1;
+    println!("x = {}", x);
+}
+
+fn section6() {
+    const MAX: u32 = 100000;
+    println!("{}", MAX);
 }
